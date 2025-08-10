f3847a39f728e1caa9743100c2b6162178091a9e
rushtamizha <159513587+rushtamizha@users.noreply.github.com>
2025-08-10 16:44:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -21,7 +21,8 @@ dotenv.config({ path: path.resolve(__dirname, '.env') });
 console.log("KEY CHECK:", process.env.RAZORPAY_KEY_ID, process.env.RAZORPAY_KEY_SECRET);
 
 const app = express();
-app.use(cors());
+import cors from "cors";
+app.use(cors({ origin: "https://mappi.onrender.com" }));
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
