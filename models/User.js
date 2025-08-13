a409b8960e0897ae4d3b7bc02e184e8e656936d6
rushtamizha <159513587+rushtamizha@users.noreply.github.com>
2025-08-13 13:14:10 +0530

    Update User.js

--- models/User.js
+++ models/User.js
@@ -4,7 +4,7 @@ const userSchema = new mongoose.Schema({
   username: {
     type: String,required: true,unique: true,trim: true
   },
-   googleId: { type: String },
+  googleId: { type: String },
   gmail: {type: String,required: true },
   password: String,
   bio: String,
@@ -15,8 +15,9 @@ const userSchema = new mongoose.Schema({
 },
   profilePic: { type: String, default: '' },
   referralCode: String,
- referredBy:{type: String},
-verified: { type: Boolean, default: false },
+  referredBy:{type: String},
+  planExpiry: { type: Date, default: null }, 
+  verified: { type: Boolean, default: false },
   wallet: { type: Number, default: 0 },
   role: { type: String, default: "user" }
 });
